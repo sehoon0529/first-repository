07c2e096df6515f8fd8dc98b61833333cb1b79df
sehoon0529 <kim44415953@gmail.com>
2023-09-14 11:22:12 +0900

    four files upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+def print_hello(a,b):
+    for i in range(a,b):
+    print("Hello World!!")
+def print_KIT():
+print("Kumoh National Institute of Technology."
+def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print("KIT")
+if __name__ == '__main__’:
+print_KIT()
+print( print_hello(1,2) )
